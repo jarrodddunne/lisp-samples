1344cf271ad565b8ca629c08d5115d1548a573b2
jarrodddunne <jarrodddunne@gmail.com>
2017-12-18 14:59:34 -0600

    Added bubble-sort

--- sorting.lisp
+++ sorting.lisp
@@ -17,9 +17,32 @@
 (defun rand-list (max-int length)
   (repeat (lambda () (floor (random max-int))) length))
 
+; Determines if a arr is sorted
+(defun sorted? (arr)
+  (if (> 2 (list-length arr))
+    t
+    (and
+     (<= (car arr) (cadr arr))
+     (sorted? (cdr arr)))))
+
+
 ; Returns the array sorted using insertion sort
 (defun insertion-sort (arr)
   (if (eq nil arr)
     nil
     (let ((min-val (reduce #'min arr)))
       (cons min-val (insertion-sort (remove-val arr min-val))))))
+
+; Fixes the first inversion in an array
+(defun fix-inversion (arr)
+  (if (> 1 (list-length arr))
+    arr
+    (if (<= (car arr) (cadr arr))
+      (append (list (car arr)) (fix-inversion (cdr arr)))
+      (append (list (cadr arr)) (list (car arr)) (cddr arr)))))
+
+; Returns the array sorted using bubble sort
+(defun bubble-sort (arr)
+  (if (sorted? arr)
+    arr
+    (bubble-sort (fix-inversion arr))))
